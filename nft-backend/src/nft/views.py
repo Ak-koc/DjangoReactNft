809736e26af56da92d747098423759fdb4ffd23e
cis1 <abdulkadir0916@gmail.com>
2022-12-19 20:02:33 +0300

    user registration and user login endpoints added

--- nft-backend/src/nft/views.py
+++ nft-backend/src/nft/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+import json
+from datetime import datetime
 
+from django.db import transaction
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.models import User
 # Create your views here.
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+from .serializers import CustomTokenObtainPairSerializer
+
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
+
+
+@transaction.atomic # if something get wrong transaction do not accept to save record in database
+@csrf_exempt
+def user_registration(request):
+    data = json.loads(request.body.decode('utf-8'))
+    first_name = data['first_name']
+    last_name = data['last_name']
+    password = data['password']
+    username = data['username']
+    email = data['email']
+
+    new_user = User.objects.create_user(username, email, password)
+    new_user.first_name = first_name
+    new_user.last_name = last_name
+    new_user.date_joined = datetime.now()
+    new_user.save()
+
+    return HttpResponse(json.dumps({"result": "SUCCESS"}), content_type="application/json")
+
