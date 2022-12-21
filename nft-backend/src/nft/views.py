933874124153fce69ad92fa58202ade911efe903
cis1 <abdulkadir0916@gmail.com>
2022-12-21 07:24:16 +0300

    password and email check in user registration

--- nft-backend/src/nft/views.py
+++ nft-backend/src/nft/views.py
@@ -1,13 +1,13 @@
 import json
 from datetime import datetime
 
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError
 from django.db import transaction
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.models import User
-# Create your views here.
 from rest_framework_simplejwt.views import TokenObtainPairView
-
 from .serializers import CustomTokenObtainPairSerializer
 
 
@@ -15,17 +15,32 @@ class CustomTokenObtainPairView(TokenObtainPairView):
     serializer_class = CustomTokenObtainPairSerializer
 
 
-@transaction.atomic # if something get wrong transaction do not accept to save record in database
+@transaction.atomic  # if something get wrong transaction do not accept to save record in database
 @csrf_exempt
 def user_registration(request):
     data = json.loads(request.body.decode('utf-8'))
     first_name = data['first_name']
     last_name = data['last_name']
     password = data['password']
-    username = data['username']
+    re_password = data['re_password']
     email = data['email']
+    username = email
 
-    new_user = User.objects.create_user(username, email, password)
+    if re_password != password:
+        return HttpResponse(json.dumps({"result": "Passwords do not match"}), content_type='application/json')
+
+    try:
+        validate_password(password)
+
+    except ValidationError:
+        raise ValidationError("Password does not validate")
+
+    try:
+        new_user = User.objects.create_user(username, email, password)
+
+    except ValidationError:
+        raise ValidationError("This email is already in use")
+
     new_user.first_name = first_name
     new_user.last_name = last_name
     new_user.date_joined = datetime.now()
